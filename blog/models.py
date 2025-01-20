75cb32d0bd3524b5dc6230def68bc19cd1770536
Pradyumna <109955188+Pradyumna1218@users.noreply.github.com>
2025-01-20 23:14:41 +0545

    Database Integration
    Used Databases in Django

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
+
+
+'''
+DATETIMEFIELD
+auto_now = True 
+    update the date time everytime the post was updated
+
+auto_now_add = True 
+    update the date time only when the object is created but you can't update the date
+
+user delete what do
+cascade post delete then user no delete but user delete post also delete
+
+MIGRATIONS
+make changes to database even after it created and has data in database
+We would have to run a complicated sql but migrations makes changes for us
+'''
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Pradyumna',
-        'title': 'Blog Post 1',
-        'content': 'First Post Created',
-        'date_posted': 'Jan 20, 2025'
-    },
-    {
-        'author': 'Kriti',
-        'title': 'Blog Post 2',
-        'content': 'My GU',
-        'date_posted': 'Jan 25, 2025'
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
